8ab1e920988f74a35963ea22afb1de5bd5ae5fa1
Youm <layard3@gmail.com>
2024-07-19 09:57:24 +0800

    feat(validator): add form validator

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,11 +6,10 @@ import (
 	"html/template"
 	"net/http"
 	"strconv"
-	"strings"
-	"unicode/utf8"
 
 	"go.uber.org/zap"
 	"pinkamkak.com/web/internal/models"
+	"pinkamkak.com/web/internal/validator"
 )
 
 type customHandler struct{}
@@ -29,10 +28,10 @@ type application struct {
 }
 
 type snippetCreateForm struct {
-	FieldErrors map[string]string
-	Title       string
-	Content     string
-	Expires     int
+	validator.Validator
+	Title   string
+	Content string
+	Expires int
 }
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -95,24 +94,14 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 
 	form := snippetCreateForm{
 		Title: r.PostForm.Get("title"), Content: r.PostForm.Get("content"), Expires: expires,
-		FieldErrors: map[string]string{},
 	}
 
-	if strings.TrimSpace(form.Title) == "" {
-		form.FieldErrors["title"] = "This field cannot be blank"
-	} else if utf8.RuneCountInString(form.Title) > 100 {
-		form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
-	}
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+	form.CheckField(validator.PermittedValue(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
 
-	if strings.TrimSpace(form.Content) == "" {
-		form.FieldErrors["content"] = "This field cannot be blank"
-	}
-
-	if expires != 1 && expires != 7 && expires != 365 {
-		form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
-	}
-
-	if len(form.FieldErrors) > 0 {
+	if form.Valid() {
 		data := app.newTemplateData(r)
 		data.Form = form
 		app.render(w, r, http.StatusUnprocessableEntity, "create.tmpl", data)
